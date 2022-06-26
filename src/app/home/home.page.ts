710b399ea6edebe328990b054857ebdf1439fda0
Matthieu Mota <matthieumota@gmail.com>
2022-06-26 10:57:58 +0200

    Search

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,11 +8,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   movies: any = [];
+  showSearch: boolean = false;
+  search: string = '';
 
   constructor(private http: HttpClient) {}
 
   ionViewWillEnter() {
     this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=ebc0a4ad59da5f80113ec7d1142c72a7')
       .subscribe(response => this.movies = response['results']);
+  }
+
+  onSearch() {
+    if (this.search) {
+      this.http.get('https://api.themoviedb.org/3/search/movie?api_key=ebc0a4ad59da5f80113ec7d1142c72a7&query='+this.search)
+        .subscribe(response => this.movies = response['results']);
+    } else {
+      this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=ebc0a4ad59da5f80113ec7d1142c72a7')
+        .subscribe(response => this.movies = response['results']);
+    }
   }
 }
